4be0338b0c0415f3c0232893b8fa20dd42dc789d
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-09-22 10:36:29 +0100

    Update getall.js

--- src/src/pages/api/docs/search/getall.js
+++ src/src/pages/api/docs/search/getall.js
@@ -1,6 +1,11 @@
 const getall = require('../../../../../lib/getrepo/getall');
 
 export default async function handler(req, res) {
+    // Allow cors
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+
     let githubToken = process.env.GITHUB_TOKEN;
     let owner = process.env.GITHUB_OWNER;
     let repo = process.env.GITHUB_REPO;
